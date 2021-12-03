21267477eee6b47f3f5faf7c3329ba4500006fff
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-12-03 09:38:54 +0000

    Simplify test

--- Tests/Ummati.Infrastructure.Test/AzureContainerAppStackTest.cs
+++ Tests/Ummati.Infrastructure.Test/AzureContainerAppStackTest.cs
@@ -2,9 +2,6 @@ namespace Ummati.Infrastructure.Test;
 
 using System.Collections.Immutable;
 using Pulumi;
-using Pulumi.AzureNative.OperationalInsights;
-using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Web;
 using Pulumi.Utilities;
 using Xunit;
 
@@ -18,22 +15,24 @@ public class AzureContainerAppStackTest
         foreach (var resource in resources)
         {
             var tagsOutput = GetTags(resource);
-            var tags = await OutputUtilities.GetValueAsync(tagsOutput).ConfigureAwait(false);
-            Assert.NotNull(tags);
-            Assert.Equal("d", tags![TagName.Application]);
-            Assert.Equal("d", tags![TagName.Environment]);
+            if (tagsOutput is not null)
+            {
+                var tags = await OutputUtilities.GetValueAsync(tagsOutput).ConfigureAwait(false);
+                Assert.NotNull(tags);
+                Assert.Equal("d", tags![TagName.Application]);
+                Assert.Equal("d", tags![TagName.Environment]);
+            }
         }
     }
 
-    private static Output<ImmutableDictionary<string, string>?> GetTags(Pulumi.Resource resource) =>
-        resource switch
+    private static Output<ImmutableDictionary<string, string>?>? GetTags(Resource resource)
+    {
+        var tagsProperty = resource.GetType().GetProperty("Tags");
+        if (tagsProperty?.PropertyType == typeof(Output<ImmutableDictionary<string, string>?>))
         {
-            ResourceGroup resourceGroup => resourceGroup.Tags,
-            Workspace workspace => workspace.Tags,
-            KubeEnvironment kubeEnvironment => kubeEnvironment.Tags,
-            ContainerApp containerApp => containerApp.Tags,
-            _ => throw new ArgumentException(
-                $"Resource with type '{resource.GetType().Name}' not recognized",
-                nameof(resource)),
-        };
+            return (Output<ImmutableDictionary<string, string>?>?)tagsProperty.GetValue(null);
+        }
+
+        return null;
+    }
 }
